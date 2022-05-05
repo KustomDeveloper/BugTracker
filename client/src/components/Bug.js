bd2c32e5ee883bb3fff02bbf5455436ea97f7fec
Michael Hicks <kustomdesigner@gmail.com>
2022-05-05 07:51:25 -0500

    Hide/Show Save Buttons

--- client/src/components/Bug.js
+++ client/src/components/Bug.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import { logOutUser } from '../actions';
 import { useDispatch } from 'react-redux';
 import Header from './Header';
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 
 const Bug = () => {
     const [bugDetails, updateBugDetails] = useState([]);
+    const [bugTitle, updateBugTitle] = useState("");
     const [bugDescription, updateBugDescription] = useState("");
 
     const token = localStorage.getItem('token');
@@ -13,8 +14,33 @@ const Bug = () => {
 
     const { id } = useParams();
 
-    const saveDescription = () => {
-        console.log('saved');
+    const titleArea = useRef();
+    const descriptionArea = useRef();
+    const textAreaBtn = useRef();
+    const headlineBtn = useRef();
+
+
+    const saveTitle = (e) => {
+        e.preventDefault();
+        console.log('Title saved');
+        headlineBtn.current.classList.remove('show-btn');
+    }
+
+    const saveDescription = (e) => {
+        e.preventDefault();
+        console.log('Description saved');
+        textAreaBtn.current.classList.remove('show-btn');
+    }
+
+    const showTitleBtn = () => {
+        if (document.activeElement === titleArea.current) {
+            headlineBtn.current.classList.add('show-btn');
+        }
+    }
+    const showDescBtn = () => {
+        if (document.activeElement === descriptionArea.current) {
+            textAreaBtn.current.classList.add('show-btn');
+        }
     }
 
     useEffect(() => {
@@ -30,6 +56,7 @@ const Bug = () => {
     
             if(data.authenticated === true) {
                 updateBugDetails(data.bug[0]);
+                updateBugTitle(data.bug[0].bug_name);
                 updateBugDescription(data.bug[0].bug_description);
             }
     
@@ -40,13 +67,18 @@ const Bug = () => {
         <>
         <Header />
         <div className="bug-card">
-            <h1>{bugDetails.bug_name}</h1>
-            {/* <form> */}
+          
+            <form>
               <div className="form-group">
-                <textarea className="form-control" value={bugDescription} onChange={e => updateBugDescription(e.target.value)}></textarea>
-                <button onClick={saveDescription} className="form-control bug-save">Save</button>
+                <input onFocus={showTitleBtn} ref={titleArea} className="form-control" value={bugTitle} onChange={e => updateBugTitle(e.target.value)}></input>
+                <button ref={headlineBtn} onClick={saveTitle} className="form-control bug-title-save">Save</button>
               </div>
-            {/* </form> */}
+                
+              <div className="form-group">
+                <textarea onFocus={showDescBtn} ref={descriptionArea} className="form-control" value={bugDescription} onChange={e => updateBugDescription(e.target.value)}></textarea>
+                <button ref={textAreaBtn} onClick={saveDescription} className="form-control bug-desc-save">Save</button>
+              </div>
+            </form>
             
         </div>
         </>
