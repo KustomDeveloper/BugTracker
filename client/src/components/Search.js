00e25755fd701b038f2a1596dfa3b058c80830a8
Michael Hicks <kustomdesigner@gmail.com>
2022-08-19 18:00:50 -0500

    Fixe link issue

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,12 +1,11 @@
 import React, {useEffect, useState} from "react";
 import { logOutUser } from '../actions';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Search = ({ allBugs, username }) => {
     const [keyword, setKeyword] = useState('');
     const [myBugs, setMyBugs] = useState([]);
-
-
 
     const dispatch = useDispatch(); 
     const token = localStorage.getItem('token');
@@ -29,21 +28,6 @@ const Search = ({ allBugs, username }) => {
         });
     }, [])
 
-    const searchBugs = (e) => {
-        e.preventDefault();
-        setKeyword(e.target.value);
-
-        // console.log(Array.isArray(myBugs))
-        // console.log(keyword);
-        // console.log(myBugs);
-
-        /*  See if bug title or bug description contains keyword  */
-
-            // const filteredBugs = myBugs.filter(myBugs.bug_name )
-            // console.log(myBugs.filter(bugs=>bugs.bug_name.toLowerCase().includes(keyword)))
-
-    }
-
   return(
     <>
     <form className="search-form">
@@ -52,17 +36,17 @@ const Search = ({ allBugs, username }) => {
             className="search" 
             type="search" 
             placeholder="Search by name, Bugs or Other" />
-        {keyword ? 
+        {keyword && 
           <i onClick={e => setKeyword('')} className="fa fa-times-circle-o clear-searchfield" aria-hidden="true"></i> 
-        : null}
+        }
     </form>
 
-    <ul className="search-dropdown" style={{padding: keyword ? '10px 0' : '0'}}>
-        {keyword ? myBugs.filter(bugs=>bugs.bug_name.toLowerCase().includes(keyword)).map((item) => (
+    <ul className="search-dropdown" style={{padding: keyword && '10px 0'}}>
+        {keyword && myBugs.filter(bugs=>bugs.bug_name.toLowerCase().includes(keyword)).map((item) => (
             <li key={item._id} className="bug-list-item">
-                <a href={'/bug/' + item._id}>{item.bug_name}</a>
+                <Link to={'/bug/' + item._id}>{item.bug_name}</Link>
             </li>
-        )) : null} 
+        ))} 
     </ul>
     </>
   )
