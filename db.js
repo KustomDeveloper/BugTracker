6fdb1ea5a721d02c19e691c4fffa41f4aac8cc1d
Michael Hicks <kustomdesigner@gmail.com>
2022-09-08 11:32:25 -0500

    Trying to fix heroku deploy

--- db.js
+++ db.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
-// const config = require('./config');
 require('dotenv').config();
 const path = require('path');
-
-
-// const { local_db: { host, port, name } } = config;
-// const connectionString = `mongodb://${host}:${port}/${name}`;
 
 const connectionString = process.env.MONGO_URI;
 
@@ -16,16 +11,6 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-
-/*
-*   Test connection
-*/
-// const test = mongoose.connection;
-
-// test.on("error", console.error.bind(console, "connection error: "));
-// test.once("open", function () {
-//   console.log("Connected successfully");
-// });
 
 // Mongo Shell
 // mongo
