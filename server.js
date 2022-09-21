f17d0e288b2284b8b21e3dc2117a6cce14d97ca0
Michael Hicks <kustomdesigner@gmail.com>
2022-09-21 12:30:36 -0500

    Testing new heroku settings

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use(Router);
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
 
-if(process.env.NODE_ENV !== "local" ) {
-    app.use(express.static(path.resolve(__dirname, "./client/build")));
+if(process.env.NODE_ENV === "production" ) {
+    //set static folder
+    app.use(express.static('client/build'));
 
-    app.use(express.static("client/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "./client/build" , "index.html"));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
